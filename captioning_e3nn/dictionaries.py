46aa163e313d4e222e246710724a103ea89ec7c6
Daniil <daniil.emcev.ru@yandex.ru>
2020-06-15 17:15:00 +0200

    temporal attention fixes

--- captioning_e3nn/dictionaries.py
+++ captioning_e3nn/dictionaries.py
@@ -17,5 +17,6 @@ atom_most_common = {"C": 0,
 dict_atoms_simple = {"C": 1,"H": 2,"N": 3, "O": 4, "S": 5, "P": 6, "Zn": 7, "Cl": 8, "F": 9, "Mg": 10, "Ca": 11, "Na": 12, "Mn": 13, "I": 14,"Br": 15,"Fe": 16, "Cu": 17, "Cd": 18, "Ni": 19, "Co": 20, "Hg": 21, "K": 22}     
 
 dict_atoms_mass = {}
+dict_atoms_hot = {}
 
 
