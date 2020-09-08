c521ba05873be0c69fff2f9ff04aba9743cf7194
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-08 12:00:21 +0200

    new trainer with masks option

--- captioning_e3nn/config.py
+++ captioning_e3nn/config.py
@@ -3,7 +3,7 @@ import yaml
 from torchvision import transforms
 from encoder import encoder_dict
 from decoder import decoder_dict
-from captioning_e3nn import data_loader
+import data_loader
 from data_loader import collate_fn, collate_fn_masks
 
 # General config
