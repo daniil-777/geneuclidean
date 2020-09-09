cf3986e313505d18143c7df6417d81832d289cf2
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-09 11:42:40 +0200

    class for training

--- captioning_e3nn/training/train.py
+++ captioning_e3nn/training/train.py
@@ -1,70 +1,34 @@
-DATA_PATH = os.path.realpath(os.path.dirname(__file__))
-N_SPLITS = 5
+import multiprocessing
 
-# args = str(sys.argv[1])
-# print(args)
+import numpy as np
+from numpy import savetxt
+import torch
 
-# Arguments
-parser = argparse.ArgumentParser(
-    description='Train a 3D reconstruction model.'
-)
-parser.add_argument('config', type=str, help='Path to config file.')
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ExponentialLR
+# from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
 
-args = parser.parse_args()
+from utils import Utils
+import argparse
+import sys
+import config
+from py3nvml import py3nvml
 
+import json
+import os
+import pickle
 
-cfg = config.load_config(args.config, 'configurations/config_lab/default.yaml')
+from sklearn.model_selection import KFold
+import numpy as np
 
-
-# configuration = utils.parse_configuration()
-
-# model params
-num_epochs = cfg['model_params']['num_epochs']
-batch_size = cfg['model_params']['batch_size']
-learning_rate = cfg['model_params']['learning_rate']
-num_workers = cfg['model_params']['num_workers']
-
-# training params
-protein_dir = cfg['training_params']['image_dir']
-caption_path = cfg['training_params']['caption_path']
-log_step = cfg['training_params']['log_step']
-save_step = cfg['training_params']['save_step']
-vocab_path = cfg['preprocessing']['vocab_path']
-
-#output files
-savedir = cfg['output_parameters']['savedir']
-tesnorboard_path = savedir
-model_path = os.path.join(savedir, "models")
-log_path = os.path.join(savedir, "logs")
-
-# Device configuration
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-Encoder, Decoder = config.get_model(cfg, device=device)
-
-nparameters_enc = sum(p.numel() for p in Encoder.parameters())
-nparameters_dec = sum(p.numel() for p in Decoder.parameters())
-print(Encoder)
-print('Total number of parameters: %d' % (nparameters_enc + nparameters_dec))
-
-if not os.path.exists(log_path):
-    os.makedirs(log_path)
-
-if not os.path.exists(model_path):
-    os.makedirs(model_path)
-
-    
-test_idx_file = open(os.path.join(log_path, "test_idx.txt"), "w")
-log_file = open(os.path.join(log_path, "log.txt"), "w")
-log_file_tensor = open(os.path.join(log_path, "log_tensor.txt"), "w")
-writer = SummaryWriter(tesnorboard_path)
-
-
-
-# Load vocabulary wrapper
-with open(vocab_path, "rb") as f:
-    vocab = pickle.load(f)
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+from build_vocab import Vocabulary
+from data_loader import get_loader, Pdb_Dataset, collate_fn, collate_fn_masks
+from models_new import DecoderRNN, Encoder_se3ACN, MyDecoderWithAttention
 
 
 
@@ -82,6 +46,7 @@ class Trainer():
         self.log_step = cfg['training_params']['log_step']
         self.save_step = cfg['training_params']['save_step']
         self.vocab_path = cfg['preprocessing']['vocab_path']
+        self.n_splits = cfg['training_params']['n_splits']
 
         #output files
         self.savedir = cfg['output_parameters']['savedir']
@@ -96,6 +61,12 @@ class Trainer():
         self.log_file_tensor = open(os.path.join(log_path, "log_tensor.txt"), "w")
         self.writer = SummaryWriter(tesnorboard_path)
         self.Encoder, self.Decoder = config.get_model(cfg, device=device)
+
+        #print all params
+        nparameters_enc = sum(p.numel() for p in Encoder.parameters())
+        nparameters_dec = sum(p.numel() for p in Decoder.parameters())
+        print('Total number of parameters: %d' % (nparameters_enc + nparameters_dec))
+
         with open(vocab_path, "rb") as f:
             vocab = pickle.load(f)
 
@@ -173,12 +144,12 @@ class Trainer():
         data_ids = np.array([i for i in range(20)])
 
         #cross validation
-        kf = KFold(n_splits=5, shuffle=True, random_state=2)
+        kf = KFold(n_splits=n_splits, shuffle=True, random_state=2)
         my_list = list(kf.split(data_ids))
         test_idx = []
         # output memory usage
         py3nvml.nvmlInit()
-        for split_no in range(N_SPLITS):
+        for split_no in range(n_splits):
             train_id, test_id = my_list[split_no]
             train_data = data_ids[train_id]
             test_data = data_ids[test_id]
