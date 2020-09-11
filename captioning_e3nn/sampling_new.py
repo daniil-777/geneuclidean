730b7193687a91811daf086c14542d10dda46b98
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-11 11:44:38 +0200

    sampling

--- captioning_e3nn/sampling_new.py
+++ captioning_e3nn/sampling_new.py
@@ -2,6 +2,40 @@
 import argparse
 import config
 from sampling.sampler import Sampler
+
+import argparse
+import config
+import multiprocessing
+
+import numpy as np
+from numpy import savetxt
+import torch
+
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ExponentialLR
+# from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+from utils import Utils
+import argparse
+import sys
+import config
+from py3nvml import py3nvml
+
+import json
+import os
+import pickle
+
+from sklearn.model_selection import KFold
+import numpy as np
+
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+from build_vocab import Vocabulary
+from data_loader import get_loader, Pdb_Dataset, collate_fn, collate_fn_masks
+from sampling.sampler import Sampler
 # from utils import Utils
 
 
