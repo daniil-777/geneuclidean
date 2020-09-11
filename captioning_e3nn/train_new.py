b246b507b5a51cbbfdc65c3b7353b445695274e5
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-11 11:35:07 +0200

    train_new

--- captioning_e3nn/train_new.py
+++ captioning_e3nn/train_new.py
@@ -1,8 +1,41 @@
 
 import argparse
 import config
+import multiprocessing
 
-from training.train import *
+import numpy as np
+from numpy import savetxt
+import torch
+
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ExponentialLR
+# from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+from utils import Utils
+import argparse
+import sys
+import config
+from py3nvml import py3nvml
+
+import json
+import os
+import pickle
+
+from sklearn.model_selection import KFold
+import numpy as np
+
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+from build_vocab import Vocabulary
+from data_loader import get_loader, Pdb_Dataset, collate_fn, collate_fn_masks
+from models_new import DecoderRNN, Encoder_se3ACN, MyDecoderWithAttention
+from training.trainer import train_loop, train_loop_mask
+from training.train import Trainer
+# from utils import Utils
+
 # from training.train import Trainer
 # from utils import Utils
 
