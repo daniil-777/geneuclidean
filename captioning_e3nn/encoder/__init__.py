7c7bf80f6702b3f651c2c1dcc7efd82562f4ba34
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-12 13:59:20 +0200

    init encoder just se3cnn

--- captioning_e3nn/encoder/__init__.py
+++ captioning_e3nn/encoder/__init__.py
@@ -1,18 +1,19 @@
 ## pointplanenet_resnet authors: Daniil Emtsev and Dusan Svilarkovic
-from encoder.encoder_resnet import ResnetPointnet, Encoder_Resnet_after_se3ACN, Encoder_Resnet_feat_geom_se3ACN, Encoder_Resnet_geom_se3ACN,  Encoder_Resnet
+# from encoder.encoder_resnet import ResnetPointnet, Encoder_Resnet_after_se3ACN, Encoder_Resnet_feat_geom_se3ACN, Encoder_Resnet_geom_se3ACN,  Encoder_Resnet
 
 from encoder.se3cnn import Encoder_se3ACN, Encoder_se3ACN_Fast
-from encoder.e3nn import Network, ResNetwork, OutputScalarNetwork, OutputMLPNetwork
+# from encoder.e3nn import Network, ResNetwork, OutputScalarNetwork, OutputMLPNetwork
 
 encoder_dict = {
-    'resnet': Encoder_Resnet,
+    # 'resnet': Encoder_Resnet,
     'se3cnn': Encoder_se3ACN,
-    'se3cnnfast': Encoder_se3ACN_Fast,
-    'se3cnn_geo_resnet': Encoder_Resnet_geom_se3ACN,
-    'se3cnn_geo_feat_resnet': Encoder_Resnet_feat_geom_se3ACN,
-    'se3cnn_resnet_after_se3cnn': Encoder_Resnet_after_se3ACN,
-    'network1': Network,
-    'resnetnetwork1': ResNetwork,
-    'OutputScalarNetwork': OutputScalarNetwork,
-    'OutputMLPNetwork': OutputMLPNetwork
 }
+#     'se3cnnfast': Encoder_se3ACN_Fast,
+#     'se3cnn_geo_resnet': Encoder_Resnet_geom_se3ACN,
+#     'se3cnn_geo_feat_resnet': Encoder_Resnet_feat_geom_se3ACN,
+#     'se3cnn_resnet_after_se3cnn': Encoder_Resnet_after_se3ACN,
+#     'network1': Network,
+#     'resnetnetwork1': ResNetwork,
+#     'OutputScalarNetwork': OutputScalarNetwork,
+#     'OutputMLPNetwork': OutputMLPNetwork
+# }
