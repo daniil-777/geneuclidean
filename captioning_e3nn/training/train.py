b244b5c6a3713b1c36a82f9192e2565ca7490813
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-14 16:27:00 +0200

    shape archtecture

--- captioning_e3nn/training/train.py
+++ captioning_e3nn/training/train.py
@@ -76,12 +76,16 @@ class Trainer():
         
         self.Encoder, self.Decoder = config.get_model(cfg, device=self.device)
         self.input = config.get_shape_input(self.cfg)
-        print(summary(self.Encoder, self.input.to(self.device)))
-        print(summary(self.Decoder))
+        # print(summary(self.Encoder, self.input))
+        # print(summary(self.Decoder))
+        print(self.Encoder)
+        print(self.Decoder)
         with open(os.path.join(self.log_path, "model.txt"), 'w') as f:
             sys.stdout = f # Change the standard output to the file we created.
-            print(summary(self.Encoder, self.input))
-            print(summary(self.Decoder))
+            # print(summary(self.Encoder, self.input))
+            # print(summary(self.Decoder))
+            print(self.Encoder)
+            print(self.Decoder)
             sys.stdout = self.original_stdout
            
         # print(model)
