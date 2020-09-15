de152d9a1c9caec1695128168fbd2d5a148ef70d
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-15 19:11:01 +0200

    sample prob decoder

--- captioning_e3nn/decoder/decoder.py
+++ captioning_e3nn/decoder/decoder.py
@@ -533,7 +533,7 @@ class MyDecoderWithAttention(nn.Module):
         sampled_ids = torch.stack(sampled_ids, 1) 
         return sampled_ids
 
-    def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=3):
+    def caption_image_beam_search(self, features, beam_size=3):
         """
         Reads an image and captions it with beam search.
 
@@ -546,37 +546,20 @@ class MyDecoderWithAttention(nn.Module):
         """
 
         k = beam_size
-        vocab_size = len(word_map)
+        vocab_size = len(self.vocab)
 
-        # Read image and process
-        img = imread(image_path)
-        if len(img.shape) == 2:
-            img = img[:, :, np.newaxis]
-            img = np.concatenate([img, img, img], axis=2)
-        img = imresize(img, (256, 256))
-        img = img.transpose(2, 0, 1)
-        img = img / 255.
-        img = torch.FloatTensor(img).to(device)
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                        std=[0.229, 0.224, 0.225])
-        transform = transforms.Compose([normalize])
-        image = transform(img)  # (3, 256, 256)
+      
 
-        # Encode
-        image = image.unsqueeze(0)  # (1, 3, 256, 256)
-        encoder_out = encoder(image)  # (1, enc_image_size, enc_image_size, encoder_dim)
-        enc_image_size = encoder_out.size(1)
-        encoder_dim = encoder_out.size(3)
 
-        # Flatten encoding
-        encoder_out = encoder_out.view(1, -1, encoder_dim)  # (1, num_pixels, encoder_dim)
-        num_pixels = encoder_out.size(1)
+        # # Flatten encoding
+        # encoder_out = encoder_out.view(1, -1, encoder_dim)  # (1, num_pixels, encoder_dim)
+        # num_pixels = encoder_out.size(1)
 
-        # We'll treat the problem as having a batch size of k
-        encoder_out = encoder_out.expand(k, num_pixels, encoder_dim)  # (k, num_pixels, encoder_dim)
+        # # We'll treat the problem as having a batch size of k
+        # encoder_out = encoder_out.expand(k, num_pixels, encoder_dim)  # (k, num_pixels, encoder_dim)
 
         # Tensor to store top k previous words at each step; now they're just <start>
-        k_prev_words = torch.LongTensor([[word_map['<start>']]] * k).to(device)  # (k, 1)
+        k_prev_words = torch.LongTensor([[self.vocab.word2idx['<start>']]] * k).to(self.device)  # (k, 1)
 
         # Tensor to store top k sequences; now they're just <start>
         seqs = k_prev_words  # (k, 1)
@@ -585,33 +568,33 @@ class MyDecoderWithAttention(nn.Module):
         top_k_scores = torch.zeros(k, 1).to(device)  # (k, 1)
 
         # Tensor to store top k sequences' alphas; now they're just 1s
-        seqs_alpha = torch.ones(k, 1, enc_image_size, enc_image_size).to(device)  # (k, 1, enc_image_size, enc_image_size)
+        # seqs_alpha = torch.ones(k, 1, enc_image_size, enc_image_size).to(device)  # (k, 1, enc_image_size, enc_image_size)
 
         # Lists to store completed sequences, their alphas and scores
         complete_seqs = list()
-        complete_seqs_alpha = list()
+        # complete_seqs_alpha = list()
         complete_seqs_scores = list()
 
         # Start decoding
         step = 1
-        h, c = decoder.init_hidden_state(encoder_out)
+        h, c = self.init_hidden_state(features)
 
         # s is a number less than or equal to k, because sequences are removed from this process once they hit <end>
         while True:
         
-            embeddings = decoder.embedding(k_prev_words).squeeze(1)  # (s, embed_dim)  ?why should we alos use it???
+            embeddings = self.embedding(k_prev_words).squeeze(1)  # (s, embed_dim)  ?why should we alos use it???
 
-            awe, alpha = decoder.attention(encoder_out, h)  # (s, encoder_dim), (s, num_pixels)
+            awe, alpha = self.attention(encoder_out, h)  # (s, encoder_dim), (s, num_pixels)
 
             alpha = alpha.view(-1, enc_image_size, enc_image_size)  # (s, enc_image_size, enc_image_size)
             
-            gate = decoder.sigmoid(decoder.f_beta(h))  # gating scalar, (s, encoder_dim)
+            gate = self.sigmoid(decoder.f_beta(h))  # gating scalar, (s, encoder_dim)
             awe = gate * awe
             #s is a batch_size_t since we do not have a batch of images, we have just one image
             # and we want to find several words. 
-            h, c = decoder.decode_step(torch.cat([embeddings, awe], dim=1), (h, c))  # (s, decoder_dim)
+            h, c = self.decode_step(torch.cat([embeddings, awe], dim=1), (h, c))  # (s, decoder_dim)
 
-            scores = decoder.fc(h)  # (s, vocab_size)
+            scores = self.fc(h)  # (s, vocab_size)
             scores = F.log_softmax(scores, dim=1)
             #!!!!!!!!!!!!!!!!!!!# choose the highest score here
             # Add
@@ -630,8 +613,8 @@ class MyDecoderWithAttention(nn.Module):
 
             # Add new words to sequences, alphas
             seqs = torch.cat([seqs[prev_word_inds], next_word_inds.unsqueeze(1)], dim=1)  # (s, step+1)
-            seqs_alpha = torch.cat([seqs_alpha[prev_word_inds], alpha[prev_word_inds].unsqueeze(1)],
-                                dim=1)  # (s, step+1, enc_image_size, enc_image_size)
+            # seqs_alpha = torch.cat([seqs_alpha[prev_word_inds], alpha[prev_word_inds].unsqueeze(1)],
+            #                     dim=1)  # (s, step+1, enc_image_size, enc_image_size)
 
             # Which sequences are incomplete (didn't reach <end>)?
             incomplete_inds = [ind for ind, next_word in enumerate(next_word_inds) if
@@ -641,7 +624,7 @@ class MyDecoderWithAttention(nn.Module):
             # Set aside complete sequences
             if len(complete_inds) > 0:
                 complete_seqs.extend(seqs[complete_inds].tolist())
-                complete_seqs_alpha.extend(seqs_alpha[complete_inds].tolist())
+                # complete_seqs_alpha.extend(seqs_alpha[complete_inds].tolist())
                 complete_seqs_scores.extend(top_k_scores[complete_inds])
             k -= len(complete_inds)  # reduce beam length accordingly
 
@@ -649,7 +632,7 @@ class MyDecoderWithAttention(nn.Module):
             if k == 0:
                 break
             seqs = seqs[incomplete_inds]
-            seqs_alpha = seqs_alpha[incomplete_inds]
+            # seqs_alpha = seqs_alpha[incomplete_inds]
             h = h[prev_word_inds[incomplete_inds]]
             c = c[prev_word_inds[incomplete_inds]]
             encoder_out = encoder_out[prev_word_inds[incomplete_inds]]
@@ -663,6 +646,6 @@ class MyDecoderWithAttention(nn.Module):
 
         i = complete_seqs_scores.index(max(complete_seqs_scores))
         seq = complete_seqs[i]
-        alphas = complete_seqs_alpha[i]
+        # alphas = complete_seqs_alpha[i]
 
-        return seq, alphas
+        return seq
