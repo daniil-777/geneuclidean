4c5056bcd0f0ee85636a20509835b310855a9ee9
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-15 12:30:52 +0200

    init in decoder fix

--- captioning_e3nn/decoder/__init__.py
+++ captioning_e3nn/decoder/__init__.py
@@ -1,10 +1,9 @@
-## pointplanenet_resnet authors: Daniil Emtsev and Dusan Svilarkovic
 from decoder.decoder import DecoderRNN, My_attention, MyDecoderWithAttention
 
 
 decoder_dict = {
     'lstm': DecoderRNN,
-    'lstm_attention': My_attention
+    'lstm_attention': MyDecoderWithAttention
 }
 
 
