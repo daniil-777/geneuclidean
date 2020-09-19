b5aa5ea6e78c4d18e2dbc2fea1bd342beafe1acd
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-19 17:00:38 +0200

    splitter

--- captioning_e3nn/split.py
+++ captioning_e3nn/split.py
@@ -75,6 +75,7 @@ class Splitter:
         self.idx_file = os.path.join(self.log_path, "idxs")
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.save_dir_smiles = os.path.join(self.savedir, "statistics")
+        self.random_state = 1337
         if not os.path.exists(self.save_dir_smiles):
             os.makedirs(self.save_dir_smiles)
         if not os.path.exists(self.log_path):
