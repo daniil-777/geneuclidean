1f0c8007dfbd37e7594fe7a238d416eafec431a8
Daniil <daniil.emcev.ru@yandex.ru>
2020-09-20 13:56:37 +0200

    binding

--- captioning_e3nn/encoder/__init__.py
+++ captioning_e3nn/encoder/__init__.py
@@ -3,6 +3,7 @@ from encoder.encoder_resnet import ResnetPointnet, Encoder_Resnet_after_se3ACN, 
 from encoder.se3cnn import Encoder_se3ACN, Encoder_se3ACN_Fast
 from encoder.e3nn import Network, ResNetwork, OutputScalarNetwork, OutputMLPNetwork
 from encoder.e3nn_vis import Network_Vis
+from encoder.binding_e3nn import Binding_Network
 
 encoder_dict = {
     'resnet': Encoder_Resnet,
@@ -15,5 +16,6 @@ encoder_dict = {
     'network1_vis': Network_Vis,
     'resnetnetwork1': ResNetwork,
     'OutputScalarNetwork': OutputScalarNetwork,
-    'OutputMLPNetwork': OutputMLPNetwork
+    'OutputMLPNetwork': OutputMLPNetwork,
+    'binding_e3nn': Binding_Network
 }
