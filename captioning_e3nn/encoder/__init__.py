e5f0a4451c27d4ff6df3a068f8e80ec16d1412e1
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-01 17:42:42 +0200

    init

--- captioning_e3nn/encoder/__init__.py
+++ captioning_e3nn/encoder/__init__.py
@@ -1,4 +1,4 @@
-from encoder.encoder_resnet import ResnetPointnet, Encoder_Resnet_after_se3ACN, Encoder_Resnet_feat_geom_se3ACN, Encoder_Resnet_geom_se3ACN,  Encoder_Resnet
+# from encoder.encoder_resnet import ResnetPointnet, Encoder_Resnet_after_se3ACN, Encoder_Resnet_feat_geom_se3ACN, Encoder_Resnet_geom_se3ACN,  Encoder_Resnet
 
 from encoder.se3cnn import Encoder_se3ACN, Encoder_se3ACN_Fast
 from encoder.e3nn import Network, ResNetwork, OutputScalarNetwork, OutputMLPNetwork
@@ -6,12 +6,6 @@ from encoder.e3nn_vis import Network_Vis
 from encoder.binding_e3nn import Binding_Network
 
 encoder_dict = {
-    'resnet': Encoder_Resnet,
-    'se3cnn': Encoder_se3ACN,
-    'se3cnnfast': Encoder_se3ACN_Fast,
-    'se3cnn_geo_resnet': Encoder_Resnet_geom_se3ACN,
-    'se3cnn_geo_feat_resnet': Encoder_Resnet_feat_geom_se3ACN,
-    'se3cnn_resnet_after_se3cnn': Encoder_Resnet_after_se3ACN,
     'network1': Network,
     'network1_vis': Network_Vis,
     'resnetnetwork1': ResNetwork,
