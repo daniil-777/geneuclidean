3fb88593f5564cfb96e1e760dc9c668b591d078b
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-01 12:17:47 +0200

    dependences.sh

--- captioning_e3nn/envs/dependences.sh
+++ captioning_e3nn/envs/dependences.sh
@@ -1,3 +1,5 @@
+conda env create -f e3nn_sampling.yml
+
 pip install torch-scatter==latest+cu101 torch-sparse==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.5.0.html
 
 pip install torch-sparse==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
