a6495eda555b05fa1517d4f76fcb8a8e46d038a2
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-01 12:11:17 +0200

    dependences.sh

--- captioning_e3nn/envs/dependences.sh
+++ captioning_e3nn/envs/dependences.sh
@@ -1,10 +1,10 @@
 pip install torch-scatter==latest+cu101 torch-sparse==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.5.0.html
 
-pip install torch-sparse==latest+$101 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
+pip install torch-sparse==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 
-pip install torch-cluster==latest+$101 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
+pip install torch-cluster==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 
-pip install torch-spline-conv==latest+$101 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
+pip install torch-spline-conv==latest+cu101 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 
 pip install torch-summary
 
