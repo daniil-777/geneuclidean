4190a240eb6155162f84c87bbda66a565a6c78c5
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-01 19:45:45 +0200

    init

--- captioning_e3nn/encoder/__init__.py
+++ captioning_e3nn/encoder/__init__.py
@@ -1,6 +1,6 @@
 # from encoder.encoder_resnet import ResnetPointnet, Encoder_Resnet_after_se3ACN, Encoder_Resnet_feat_geom_se3ACN, Encoder_Resnet_geom_se3ACN,  Encoder_Resnet
 
-from encoder.se3cnn import Encoder_se3ACN, Encoder_se3ACN_Fast
+# from encoder.se3cnn import Encoder_se3ACN, Encoder_se3ACN_Fast
 from encoder.e3nn import Network, ResNetwork, OutputScalarNetwork, OutputMLPNetwork
 from encoder.e3nn_vis import Network_Vis
 from encoder.binding_e3nn import Binding_Network
