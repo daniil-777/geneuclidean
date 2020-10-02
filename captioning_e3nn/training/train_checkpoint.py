af23e0ad6273d23d80256d0a4d39416e8c712177
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-02 16:17:10 +0200

    checkp

--- captioning_e3nn/training/train_checkpoint.py
+++ captioning_e3nn/training/train_checkpoint.py
@@ -113,11 +113,12 @@ class Trainer_Fold():
         self.model_name = 'e3nn'
         self.checkpoint_path =  os.path.join(self.model_path, 'checkpoint_' + self.model_name + '.pkl')
         if (os.path.exists(self.checkpoint_path)):
-            checkpoint = model_zoo.load_url(self.checkpoint_path)
-            #  torch.load(self.checkpoint_path)
+            checkpoint = torch.load(self.checkpoint_path)
+            
             print("loading model...")
             self.start_epoch = checkpoint['start_epoch'] + 1
             self.Encoder, self.Decoder = config.get_model(cfg, device=self.device)
+      
             self.Encoder.load_state_dict(checkpoint['encoder'])
             # self.Decoder.load_state_dict(torch.load(checkpoint['encoder'])
             self.caption_optimizer = checkpoint['caption_optimizer']
