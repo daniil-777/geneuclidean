6909653f3c1bffd7b41d0082fd071fa8353eb7a3
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-02 16:06:34 +0200

    checkp

--- captioning_e3nn/training/train_checkpoint.py
+++ captioning_e3nn/training/train_checkpoint.py
@@ -118,8 +118,8 @@ class Trainer_Fold():
             print("loading model...")
             self.start_epoch = checkpoint['start_epoch'] + 1
             self.Encoder, self.Decoder = config.get_model(cfg, device=self.device)
-            self.Encoder.load_state_dict(checkpoint['encoder']))
-            self.Decoder.load_state_dict(torch.load(checkpoint['encoder']))
+            self.Encoder.load_state_dict(checkpoint['encoder'])
+            self.Decoder.load_state_dict(torch.load(checkpoint['encoder'])
             self.caption_optimizer = checkpoint['caption_optimizer']
             self.split_no = checkpoint['split_no']
         else:
