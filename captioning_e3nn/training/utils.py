a14cf1c6fa9bc9db45ea0dd82a29cf377d7c5920
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-02 12:01:49 +0200

    utils

---
+++ captioning_e3nn/training/utils.py
@@ -0,0 +1,29 @@
+import os
+import torch
+
+
+def save_checkpoint(checkpoint_path, model_name, start_epoch, encoder, decoder,
+                    encoder_best, decoder_best, caption_optimizer, split_no):
+    """
+    Saves model checkpoint.
+    :param data_name: base name of processed dataset
+    :param epoch: epoch number
+    :param epochs_since_improvement: number of epochs since last improvement in BLEU-4 score
+    :param encoder: encoder model
+    :param decoder: decoder model
+    :param encoder_optimizer: optimizer to update encoder's weights, if fine-tuning
+    :param decoder_optimizer: optimizer to update decoder's weights
+    :param bleu4: validation BLEU-4 score for this epoch
+    :param is_best: is this checkpoint the best so far?
+    """
+    state = {'start_epoch': start_epoch,
+             'encoder': encoder,
+             'decoder': decoder,
+             'caption_optimizer': caption_optimizer,
+             'split_no': split_no
+             'encoder_best': encoder_best,
+             'decoder_best': decoder_best}
+
+    filename = os.path.join(checkpoint_path, 'checkpoint_' + model_name + '.pth.tar')
+    torch.save(state, filename)
+   
