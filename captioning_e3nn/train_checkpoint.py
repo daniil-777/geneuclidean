b75133f21aa42d3e2d3eb0d2cecc17d986317728
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-02 11:12:11 +0200

    decoder_vis

--- captioning_e3nn/train_checkpoint.py
+++ captioning_e3nn/train_checkpoint.py
@@ -36,19 +36,18 @@ from data_loader import get_loader, Pdb_Dataset, collate_fn, collate_fn_masks
 from training.train_checkpoint import Trainer_Fold
 
 
-
-def main():
-    parser = argparse.ArgumentParser(
+parser = argparse.ArgumentParser(
     description='Train a 3D reconstruction model.'
 )
-    parser.add_argument('config', type=str, help='Path to config file.')
-    parser.add_argument('model_name', type=str, default='model', help='Model output file, i.e. for stupid_name.pt insert stupid_name')
+parser.add_argument('config', type=str, help='Path to config file.')
+parser.add_argument('model_name', type=str, default='model', help='Model output file, i.e. for stupid_name.pt insert stupid_name')
 
-    args = parser.parse_args()
-    global model_name
-    ## added by Dusan
-    model_name = args.model_name
-   
+args = parser.parse_args()
+global modelname
+model_name = args.model_name
+
+
+def main():
     cfg = config.load_config(args.config, 'configurations/config_local/default.yaml')
     trainer = Trainer_Fold(cfg)
     trainer.train_epochs()
