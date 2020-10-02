7defbeb2f7565c3dff972bc1c0f706a2c251baaf
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-02 10:44:36 +0200

    checkpoint

---
+++ captioning_e3nn/train_checkpoint.py
@@ -0,0 +1,57 @@
+
+import argparse
+import config
+import multiprocessing
+
+import numpy as np
+from numpy import savetxt
+import torch
+
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ExponentialLR
+# from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+from utils import Utils
+import argparse
+import sys
+import config
+from py3nvml import py3nvml
+
+import json
+import os
+import pickle
+
+from sklearn.model_selection import KFold
+import numpy as np
+
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+from build_vocab import Vocabulary
+from data_loader import get_loader, Pdb_Dataset, collate_fn, collate_fn_masks
+from models_new import DecoderRNN, Encoder_se3ACN, MyDecoderWithAttention
+# from training.trainer import train_loop, train_loop_mask
+from training.train_checkpoint import Trainer_Fold
+
+
+
+def main():
+    parser = argparse.ArgumentParser(
+    description='Train a 3D reconstruction model.'
+)
+    parser.add_argument('config', type=str, help='Path to config file.')
+    parser.add_argument('model_name', type=str, default='model', help='Model output file, i.e. for stupid_name.pt insert stupid_name')
+
+    args = parser.parse_args()
+    global model_name
+    ## added by Dusan
+    model_name = args.model_name
+   
+    cfg = config.load_config(args.config, 'configurations/config_local/default.yaml')
+    trainer = Trainer_Fold(cfg)
+    trainer.train_epochs()
+
+if __name__ == "__main__":
+    main()
