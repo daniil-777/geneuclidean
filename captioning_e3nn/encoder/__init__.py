7c12484e6769e88294b97350ff09685f0af0d44d
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-05 14:55:24 +0200

    pointgeo local

--- captioning_e3nn/encoder/__init__.py
+++ captioning_e3nn/encoder/__init__.py
@@ -1,5 +1,5 @@
 from encoder.encoder_resnet import ResnetPointnet, Encoder_Resnet_after_se3ACN, Encoder_Resnet_feat_geom_se3ACN, Encoder_Resnet_geom_se3ACN,  Encoder_Resnet
-
+from encoder.pointnet_e3nn import PointNet_Geo_AllNetwork
 # from encoder.se3cnn import Encoder_se3ACN, Encoder_se3ACN_Fast
 from encoder.e3nn import Network, ResNetwork, OutputScalarNetwork, OutputMLPNetwork
 from encoder.e3nn_vis import Network_Vis
@@ -18,5 +18,6 @@ encoder_dict = {
     'pointnetall': PointNetAllNetwork,
     'att_e3nn': AttentionE3nn,
     'se3cnn_resnet_after_se3cnn': Encoder_Resnet_after_se3ACN,
-    'se3cnn_geo_feat_resnet': Encoder_Resnet_feat_geom_se3ACN
+    'se3cnn_geo_feat_resnet': Encoder_Resnet_feat_geom_se3ACN,
+    'pointnet_geo': PointNet_Geo_AllNetwork
 }
