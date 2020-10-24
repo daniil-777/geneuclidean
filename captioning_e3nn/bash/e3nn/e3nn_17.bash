1379a77eec5f37f861be8fff7363450e88fa92f6
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-24 15:50:31 +0200

    e3nn_17 ended

--- captioning_e3nn/bash/e3nn/e3nn_17.bash
+++ captioning_e3nn/bash/e3nn/e3nn_17.bash
@@ -1,4 +1,4 @@
-command="-n 4 -W 4:00 -R "rusage[ngpus_excl_p=1]" -R volta  -oo logs/e3nn_17  python train_captioning.py  configurations/config_lab/e3nn/e3nn_17.yaml  random 0"
+command="-n 4 -W 4:00  -R "rusage[ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]"  -oo logs/e3nn_17  python train_captioning.py  configurations/config_lab/e3nn/e3nn_17.yaml  random 0"
 bsub -J e3nn_17 $command
 for i in {1..3}; do
     bsub -J e3nn_17 -w "ended(e3nn_17)" $command
