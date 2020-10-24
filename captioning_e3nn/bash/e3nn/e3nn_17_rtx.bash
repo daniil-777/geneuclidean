5ff2f75f4442999502c95f1d6356083405d96f5c
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-24 15:34:51 +0200

    e3nn_17 ended

--- captioning_e3nn/bash/e3nn/e3nn_17_rtx.bash
+++ captioning_e3nn/bash/e3nn/e3nn_17_rtx.bash
@@ -1,4 +1,4 @@
-command="-n 10 -W 4:00 -R "rusage[ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" -R "select[gpu_mtotal0>=10240]"  -oo logs/e3nn_17_rxx  python train_captioning.py  configurations/config_lab/e3nn/e3nn_17.yaml  random 0"
+command="-n 10 -W 4:00 -R "rusage[ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]"  -oo logs/e3nn_17_rxx  python train_captioning.py  configurations/config_lab/e3nn/e3nn_17.yaml  random 0"
 bsub -J e3nn_17_rtx $command
 for i in {1..3}; do
     bsub -J e3nn_17_rtx -w "ended(e3nn_17_rtx)" $command
