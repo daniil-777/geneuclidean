969eb22f2857e808c04cde90403553129d6e8b6d
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-25 00:12:22 +0200

    ended e3nn3 e3nn 16

--- captioning_e3nn/bash/e3nn/e3nn_16.bash
+++ captioning_e3nn/bash/e3nn/e3nn_16.bash
@@ -1,5 +1,5 @@
 command="-n 4 -W 24:00 -R "rusage[mem=10000,ngpus_excl_p=1]"   -oo logs/e3nn_16  python train_captioning.py  configurations/config_lab/e3nn/e3nn_16.yaml  random 0"
 bsub -J e3nn_16 $command
 for i in {1..2}; do
-    bsub -J e3nn_16 -w "done(e3nn_16)" $command
+    bsub -J e3nn_16 -w "ended(e3nn_16)" $command
 done
