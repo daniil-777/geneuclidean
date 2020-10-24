bfcf2e754e024c4cdbdf8e8926a77552df53cd11
Daniil <daniil.emcev.ru@yandex.ru>
2020-10-25 00:03:17 +0200

    train_captioning

--- captioning_e3nn/train_captioning.py
+++ captioning_e3nn/train_captioning.py
@@ -84,7 +84,7 @@ def main():
     #             "simple_probabilistic_topk_10"]
     #sampling
     # regimes = ["probabilistic", "max", "beam_1", "beam_3", "beam_10"]
-    regimes = ["probabilistic", "beam_1", "beam_3", "beam_10", "beam_20"]
+    regimes = ["probabilistic", "max", "beam_1", "beam_3", "beam_10", "beam_20"]
     end_sampling_ind = len(regimes)
     if (os.path.exists(checkpoint_sampling_path)):
         print("loading sample ids...")
