bb20b6f2ea7f6a9a4eb84baa668e472bb9884c78
Daniil <daniil.emcev.ru@yandex.ru>
2020-11-01 18:38:06 +0100

    checkpoint features

--- src/utils/checkpoint.py
+++ src/utils/checkpoint.py
@@ -2,9 +2,9 @@ import torch
 
 def save_checkpoint_feature(checkpoint_path, idx_max_length, max_length, idx_write):
 
-  state = {'idx_max_length': idx_sampling,
+  state = {'idx_max_length': idx_max_length,
            'max_length': max_length,
-           'idx_write': idx_sample_regime_start, 
+           'idx_write': idx_write, 
           }
 
   torch.save(state, checkpoint_path)
