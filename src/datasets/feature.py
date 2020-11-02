869a236e85da1db144367f6cdc88e61864c8c088
Daniil <daniil.emcev.ru@yandex.ru>
2020-11-02 11:39:29 +0100

    parallel feature preprocessing

--- src/datasets/feature.py
+++ src/datasets/feature.py
@@ -1,4 +1,6 @@
 import os
+import multiprocessing
+from multiprocessing import Pool
 from functools import partial
 import re
 import numpy as np
@@ -16,6 +18,8 @@ from moleculekit.tools.voxeldescriptors import getChannels
 # import dictionary of atoms' types and hot encoders
 from src.datasets.dictionaries import atom_most_common, dict_atoms_hot, dict_atoms_simple, dict_atoms_masses, dict_atoms_charges
 from src.utils.checkpoint import save_checkpoint_feature
+import src.utils.config as config
+import argparse
 
 # from dict 
 class Featuring():
@@ -25,12 +29,12 @@ class Featuring():
         self.path_root = cfg['preprocessing']['path_root']
         self.path_data = cfg['data']['path']
         self.path_checkpoint = os.path.join(self.path_data,  "preprocess_checkpoint.csv")
-        self.file_checkpoint_data = open(self.path_checkpoint,  "a+")
-  
+        self.file_checkpoint_data = open(self.path_checkpoint,  "a+").close()
+        # self.file_checkpoint_data.close()
         if (len(open(self.path_checkpoint).readlines()) == 0):
-            self.file_checkpoint_data.write('radious,type_feature,type_filtering,h_filterig'+ "\n")
-            self.file_checkpoint_data.flush()
-
+            print("creating the file...")
+            with open(self.path_checkpoint,  "a+") as f:
+                f.write('radious,type_feature,type_filtering,h_filterig'+ "\n")
         self.init_refined = self.path_root + "/data/new_refined/"
         self.init_casf = self.path_root + "/data/new_core_2016/"
         self.dict_atoms = dict_atoms_hot
@@ -46,30 +50,61 @@ class Featuring():
         self.files_refined = os.listdir(self.init_refined)
         self.files_refined = [file for file in self.files_refined if file[0].isdigit()]
         self.files_refined.sort()
-         
-        array_names = [str(radious), self.type_feature, self.type_filtering, self.h_filterig]
-        self.name_checkpoint_features = '_'.join(array_names)
-        os.makedirs(os.path.join(self.path_data, "checkpoints"), exist_ok=True)
-        self.path_checkpoint_features = os.path.join(self.path_data, "checkpoints", self.name_checkpoint_features + ".pkl")
-        if (os.path.exists(self.path_checkpoint_features)):
-            print("loading feature ids...")
-            checkpoint_features = torch.load(self.path_checkpoint_features)
-            self.idx_max_length = checkpoint_features['idx_max_length']
-            self.max_length = checkpoint_features['max_length']
-            self.idx_write = checkpoint_features['idx_write']
-        else:
-            self.idx_max_length = 130
-            self.max_length = 0
-            self.idx_write = 0
-            save_checkpoint_feature(self.path_checkpoint_features, self.idx_max_length, self.max_length, self.idx_write)
-        
+        self.idx_files_refined = list(range(0, len(self.files_refined)))
+        # self.idx_files_refined = [0, 1]
+        self.max_length = 0
         if not self.check_featuring():
             print("calculating max length...")
-            self.max_length = 0
-            self.write_filtered_pad_feat_geo()
+            self.run_parallel_max_length()
+            print("writing to files...")
+            self.run_parallel_write()
         else:
             f, m, g = self._get_feat_geo_from_file(0)
             self.max_length = f.shape[0]
+        # array_names = [str(radious), self.type_feature, self.type_filtering, self.h_filterig]
+        # self.name_checkpoint_features = '_'.join(array_names)
+        # os.makedirs(os.path.join(self.path_data, "checkpoints"), exist_ok=True)
+        # self.path_checkpoint_features = os.path.join(self.path_data, "checkpoints", self.name_checkpoint_features + ".pkl")
+        # if (os.path.exists(self.path_checkpoint_features)):
+        #     print("loading feature ids...")
+        #     checkpoint_features = torch.load(self.path_checkpoint_features)
+        #     self.idx_max_length = checkpoint_features['idx_max_length']
+        #     self.max_length = checkpoint_features['max_length']
+        #     self.idx_write = checkpoint_features['idx_write']
+        # else:
+        #     self.idx_max_length = 130
+        #     self.max_length = 0
+        #     self.idx_write = 0
+        #     save_checkpoint_feature(self.path_checkpoint_features, self.idx_max_length, self.max_length, self.idx_write)
+
+            # self.max_length = 0
+            # self.write_filtered_pad_feat_geo()
+        # else:
+        #     f, m, g = self._get_feat_geo_from_file(0)
+        #     self.max_length = f.shape[0]
+
+    def run_parallel_write(self):
+        with Pool(processes=8) as pool:
+            pool.map(self.write_padd_feat_geo, self.idx_files_refined)
+        self.write_checkpoint()
+
+    def run_parallel_max_length(self):
+        with Pool(processes=8) as pool:
+            lengthes = pool.map(self._get_length, self.idx_files_refined) 
+        self.max_length = max(lengthes)
+        print("********max********* - ", self.max_length)
+    
+    def _get_length(self, pdb_id):
+        features_filt, geo_filt = self._get_features_geo_filtered(pdb_id)
+        length = features_filt.shape[0]
+        return length
+
+    def write_padd_feat_geo(self, id):
+        feat_filt_padded, masks, geo_filt_padded = self._get_features_geo_padded(id, self.max_length)
+        path_feature, path_mask, path_geo = self._get_name_save(id)
+        torch.save(feat_filt_padded, path_feature)
+        torch.save(masks, path_mask)
+        torch.save(geo_filt_padded, path_geo)
 
     def _get_feat_geo_from_file(self, id):
         """reads torch tensors of feature/geo from files
@@ -157,7 +192,7 @@ class Featuring():
             value=99,
         )
         return feat_filt_padded, mask_binary, geo_filt_padded
- 
+
     def _get_length_max(self):
         """get the max length of feature array among all pdbids
 
@@ -167,7 +202,6 @@ class Featuring():
         # data_list = list(range(len(self.files_refined)))
         data_list = range(self.idx_max_length, len(self.files_refined))
         progress = tqdm(data_list)
-    
         for pdb_id in progress:
             features_filt, geo_filt = self._get_features_geo_filtered(pdb_id)
             length = features_filt.shape[1]
@@ -179,7 +213,7 @@ class Featuring():
             save_checkpoint_feature(self.path_checkpoint_features, pdb_id, self.max_length, id)
         return self.max_length
 
-    def _get_features_geo_filtered(self, id):
+    def _get_features_geo_filtered(self, pdb_id):
         """calculates features and geometry with filteing
 
         Args:pdb id of a protein]
@@ -187,13 +221,9 @@ class Featuring():
         Returns:
             [torch.tensor]: [Num_atoms * Feat_dim]
         """
-        features, geometry = self._get_features_geo(id)
-        mask = self._get_mask_selected_atoms_pocket(id)
-        # print("feat shape", features.shape)
-        # print("geo shape", geometry.shape)
-        # print("mask shape", len(mask))
+        features, geometry = self._get_features_geo(pdb_id)
+        mask = self._get_mask_selected_atoms_pocket(pdb_id)
         features_filtered, geometry_filtered = features[mask, :], geometry[mask, :]
-        # print("shape feat_filt", features_filtered.shape)
         features_filtered = torch.from_numpy(features_filtered).squeeze()
         geometry_filtered = torch.from_numpy(geometry_filtered).squeeze()
         return features_filtered, geometry_filtered
@@ -272,15 +302,9 @@ class Featuring():
         Returns:
             [np.array]: [array of pharmacophoric properties [N_atoms, dim_feature]]
         """
-        #
-        # selection_mask = self.mask_selected_atoms_pocket(id)
-        # mol = Molecule(id, validateElements=False)
-        # mol.filter('protein')
         #pocket
         path_protein, _ = self._get_path(id)
         protein_name = self.files_refined[id]
-        # mol = Molecule(protein_name)
-        # mol.filter('protein')
         mol = Molecule(path_protein)
         mol.filter('protein')
         mol = prepareProteinForAtomtyping(mol, verbose = False)
@@ -288,11 +312,11 @@ class Featuring():
         features = getChannels(mol, version=2)
         features = (features[0] > 0).astype(np.float32)
         features = np.asarray(features[:, :-1])
-        print("feat shape bio - ", features.shape)
+        # print("feat shape bio - ", features.shape)
         return features
     
     def _get_mask_selected_atoms_pocket(
-        self, id_pdb: int,
+        self, pdb_id: int,
     ):
         """selects atoms of "id_pdb" protein within the distance "precision" around "center_lig"
         
@@ -306,7 +330,7 @@ class Featuring():
             Radius of atoms selections wrp center of ligand
         """
 
-        path_protein, path_ligand = self._get_path(id_pdb)
+        path_protein, path_ligand = self._get_path(pdb_id)
         center_ligand = self._get_ligand_center(path_ligand)
         if self.type_filtering == "all" and self.h_filterig == 'h':
             sel="protein and noh and sqr(x-'{0}')+sqr(y-'{1}')+sqr(z-'{2}') <= sqr('{3}')".format(
@@ -317,9 +341,9 @@ class Featuring():
             )
         elif self.type_filtering == "all" and self.h_filterig == '-h':
             sel="sqr(x-'{0}')+sqr(y-'{1}')+sqr(z-'{2}') <= sqr('{3}')".format(
-                str(center_lig[0][0]),
-                str(center_lig[0][1]),
-                str(center_lig[0][2]),
+                str(center_ligand[0][0]),
+                str(center_ligand[0][1]),
+                str(center_ligand[0][2]),
                 str(self.radious),
             )
         mol_protein = Molecule(path_protein)
@@ -402,8 +426,8 @@ class Featuring():
     def write_checkpoint(self):
         """writes inf about radious, type_feature, type_filtering, h_filterig used at extracting features/geometry of atoms
         """
-        # self.file_checkpoint_data = open(self.path_checkpoint, "a+")
-        array_to_write = [str(radious), self.type_feature, self.type_filtering, self.h_filterig]
+        self.file_checkpoint_data = open(self.path_checkpoint, "a+")
+        array_to_write = [str(self.radious), self.type_feature, self.type_filtering, self.h_filterig]
         self.file_checkpoint_data.write(','.join(array_to_write) + "\n")
         self.file_checkpoint_data.flush()
 
@@ -416,8 +440,46 @@ class Featuring():
         existing_featuring = pd.read_csv(self.path_checkpoint)
         array_to_check = [float(self.radious), self.type_feature, self.type_filtering, self.h_filterig]
         bool_answer = (existing_featuring == array_to_check).all(1).any()
+        # self.file_checkpoint_data.close()
         return bool_answer
 
+
+class Batch_prep(Featuring):
+    def __init__(self, cfg, radious, type_feature, type_filtering, h_filterig, n_proc=2, mp_pool=None):
+        super(Batch_prep, self).__init__(cfg, radious, type_feature, type_filtering, h_filterig)
+        self.mp = multiprocessing.Pool(n_proc)
+
+    def transform_data(self):
+        inputs = self.mp.map(self._get_length, self.files_refined)
+        # Sometimes representation generation fails
+        inputs = list(filter(lambda x: x is not None, inputs))
+        return max(inputs)
+
+def main():
+    parser = argparse.ArgumentParser(
+    description='Train a 3D reconstruction model.')
+    
+    parser.add_argument('--config', type=str, help='Path to config file.')
+    parser.add_argument('--radious', type=int , default=8, help='dimension of word embedding vectors')
+    parser.add_argument('--type_feature', type=str , default='mass_charge', help='type_feature')
+    parser.add_argument('--type_filtering', type=str , default = 'all', help='type_filtering')
+    parser.add_argument('--h_filterig', type=str , default='without_h', help='h')
+    parser.add_argument('--type_fold', type=str, help='type_fold')
+    parser.add_argument('--idx_fold', type=str, help='idx fold')
+    args = parser.parse_args()
+                         
+    cfg = config.load_config(args.config, 'configurations/config_local/default.yaml')
+    type_fold = args.type_fold
+    idx_fold = args.idx_fold
+    savedir = cfg["output_parameters"]["savedir"]
+    model_name = cfg["model_params"]["model_name"]
+    num_epoches = cfg["model_params"]["num_epochs"]
+    #features generation
+    Feature_gen = Featuring(cfg, args.radious, args.type_feature, args.type_filtering, args.h_filterig)
+    print("max length", Feature_gen.max_length)
+
+if __name__ == "__main__":
+    main()
 
 
      
