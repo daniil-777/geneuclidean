07d61584cc5fff65a279123cc8703ee3ed6d3a84
Daniil <daniil.emcev.ru@yandex.ru>
2020-11-14 09:59:02 +0100

    resnet 34

---
+++ src/configurations/bash/e3nn/bio_local_resnet_34_morgan.bash
@@ -0,0 +1,5 @@
+command="-n 8 -W 4:00 -R "rusage[mem=10000,ngpus_excl_p=1]"  -oo logs/hot_v_bio_local_resnet_34_morgan python train_all_folds.py --loc=lab --config=configurations/config_lab/bio_e3nn/bio_local_resnet_34.yaml --radious=8 --type_feature=hot_simple  --type_filtering=all --h_filterig=h --type_fold=morgan"
+bsub -J bio_local_resnet_34_morgan $command
+for i in {1..6}; do
+    bsub -J bio_local_resnet_34_morgan -w "ended(bio_local_resnet_34_morgan)" $command
+done
