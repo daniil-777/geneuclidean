c8e875dbe3eece16b47f50646140534b5d6f291b
Daniil <daniil.emcev.ru@yandex.ru>
2020-11-19 13:16:08 +0100

    random bio local 33 bash

--- src/configurations/bash/e3nn/bio_local_net_33_random.bash
+++ src/configurations/bash/e3nn/bio_local_net_33_random.bash
@@ -1,5 +1,5 @@
 command="-n 8 -W 4:00 -R "rusage[mem=10000,ngpus_excl_p=1]"  -oo logs/hot_v_bio_local_net_33_random python train_all_folds.py --loc=lab --config=configurations/config_lab/bio_e3nn/bio_local_net_33.yaml --radious=8 --type_feature=hot_simple  --type_filtering=all --h_filterig=h --type_fold=random"
-bsub -J bio_local_net_33_chain $command
+bsub -J bio_local_net_33_random $command
 for i in {1..6}; do
-    bsub -J bio_local_net_33_chain -w "ended(bio_local_net_33_chain)" $command
+    bsub -J bio_local_net_33_random -w "ended(bio_local_net_33_random)" $command
 done
